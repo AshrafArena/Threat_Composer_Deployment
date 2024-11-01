3958dda6f256a9c7537711ad94860b187f72fac9
Achraf Marhfour <129840697+AshrafArena@users.noreply.github.com>
2024-11-01 16:08:20 +0000

    Updated provider.tf

--- terraform/module/app/provider.tf
+++ terraform/module/app/provider.tf
@@ -4,21 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "5.0.0-alpha1"
-    }
   }
 }
-
 
 provider "aws" {
   region = var.region
 }
 
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
 
 
 
