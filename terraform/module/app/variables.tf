3a311199c0dc044be765149acd46472f1093c45e
AshrafArena <achrafreda98@outlook.com>
2024-11-01 16:07:27 +0000

    updated providers

--- terraform/module/app/variables.tf
+++ terraform/module/app/variables.tf
@@ -2,7 +2,6 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-
 variable "aws_subnet_1" {
   default = "10.0.1.0/24"
 }
@@ -16,18 +15,14 @@ variable "threat_app_container_image" {
   type        = string
 }
 
-variable "cloudflare_zone_id" {
-  description = "Cloudflare Zone ID for the domain"
-  type        = string
-}
-
-variable "cloudflare_api_token" {
-  description = "API token for Cloudflare"
+variable "route53_zone_id" {
+  description = "Route 53 Zone ID for the domain"
   type        = string
 }
 
 variable "region" {
-  description = "aws region"
+  description = "AWS region"
   type        = string
   default     = "eu-west-2"
 }
+
